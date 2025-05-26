27083c3c780ac32d4bdc065464ca10b24798ef76
iAmNotARobot <diamadex@gmail.com>
2025-05-26 21:03:00 +0800

    - fixed eslint configuration - cleanup with eslint

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,35 +1,34 @@
-class Project  {
+class Project {
   private name: string;
   private description: string;
-  private tech: Array<string>;
-  
-  public get getName() : string {
+  private tech: string[];
+  public get getName(): string {
     return this.name;
   }
-  public get getDescription() : string {
+  public get getDescription(): string {
     return this.description;
   }
 
-  public get getTech(): Array<string> {
+  public get getTech(): string[] {
     return this.tech;
   }
   public set updateName(v: string) {
-    if (v.trim() === '') throw new Error('Name cannot be empty');
+    if (v.trim() === "") throw new Error("Name cannot be empty");
     this.name = v;
   }
   public set updateDescription(v: string) {
-    if (v.trim() === '') throw new Error('Description cannot be empty');
+    if (v.trim() === "") throw new Error("Description cannot be empty");
     this.name = v;
   }
-  public set updateTech(v: Array<string>) {
+  public set updateTech(v: string[]) {
     this.tech = v;
   }
 
-  constructor(name: string, description: string, tech: Array<string>) {
+  constructor(name: string, description: string, tech: string[]) {
     this.name = name;
     this.description = description;
     this.tech = tech;
   }
 }
 
-export default Project
+export default Project;
