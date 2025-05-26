5b855efbcca674dfcb6ae9eda54d15f001aa77e2
iAmNotARobot <diamadex@gmail.com>
2025-05-26 16:46:45 +0800

    added postcss config

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const { default: tailwindcss } = require("@tailwindcss/vite");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
