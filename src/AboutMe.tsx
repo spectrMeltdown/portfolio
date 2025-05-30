07fecdb95a79cc2d5ceb2bc0e410cd37151e7e08
IAmNotARobot <38276929+spectrMeltdown@users.noreply.github.com>
2025-05-30 10:37:20 +0800

    - converted lf to crlf (i synced to git from a project i copied from linux) - refactored some ui to their own components

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -5,6 +5,8 @@ import {
   faLaptop,
   faTablet,
 } from "@fortawesome/free-solid-svg-icons";
+import Circle from "./components/ui/circle";
+import Line from "./components/ui/line";
 
 export default AboutMe;
 
@@ -15,8 +17,8 @@ function AboutMe() {
   //   ["Cross-platform", "developer.svg"],
   // ]);
   return (
-    <div className="grid grid-cols-[50%_50%] py-20">
-      <Line />
+    <div className="grid grid-cols-[50%_50%] px-10 py-20">
+      <Lines />
       <div className="col-auto">
         <h2 className="mb-5 text-4xl font-bold">About me</h2>
         <p>
@@ -31,12 +33,18 @@ function AboutMe() {
   );
 }
 
-function Line() {
-  const mobileIcon = ({ stack = false }: { stack: boolean }) => (
+function Lines() {
+  const mobileIcon = ({
+    stack = false,
+    gap = "ml-[0.8rem]",
+  }: {
+    stack?: boolean;
+    gap?: string;
+  }) => (
     <FontAwesomeIcon
       icon={faMobile}
       size={stack ? "lg" : "2xl"}
-      className={`${stack ? "mt-[0.7rem] ml-[0.8rem] rounded-[3px] border-[0.12rem] border-gray-800 bg-gray-800" : "pr-3"}`}
+      className={`${stack ? `mt-[0.7rem] ml-5 rounded-[3px] border-[0.12rem] border-gray-800 bg-gray-800` : "pr-3"}`}
     />
   );
   const laptopIcon = <FontAwesomeIcon icon={faLaptop} size={"2x"} />;
@@ -49,28 +57,28 @@ function Line() {
   );
   return (
     <div className="col-auto">
-      <div className="ml-1.5 flex h-10 items-center gap-3 border-l-2 border-primary pl-10">
+      <Line>
         <FontAwesomeIcon
           icon={faWindowMaximize}
           size={"2xl"}
-          className="pr-3"
+          className="pr-3 pl-1"
         />
         {"Website Development"}
-      </div>
-      <div className="mx-[0.22rem] my-2 size-2 rounded-full bg-primary pr-1"></div>
-      <div className="ml-1.5 flex h-10 items-center gap-4 border-l-2 border-primary pl-11">
-        {mobileIcon({ stack: false })}
+      </Line>
+      <Circle />
+      <Line>
+        <div className="pl-2">{mobileIcon({ gap: "ml-5" })}</div>
         {"App Development"}
-      </div>
-      <div className="mx-[0.22rem] my-2 size-2 rounded-full bg-primary pr-1"></div>
-      <div className="ml-1.5 flex h-10 items-center gap-2 border-l-2 border-primary pl-10">
+      </Line>
+      <Circle />
+      <Line>
         <div className="flex">
           {laptopIcon}
           {tabletIcon({ stack: true })}
           {mobileIcon({ stack: true })}
         </div>
         {"Cross-Platform Development"}
-      </div>
+      </Line>
     </div>
   );
 }
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -5,72 +5,15 @@ import {
   faLaptop,
   faTablet,
 } from "@fortawesome/free-solid-svg-icons";
+import Line from "./components/ui/line";
 
-export default AboutMe;
+export default Projects;
 
-function AboutMe() {
+function Projects() {
   // const categories = new Map<string, string>([
   //   ["Web", "developer.svg"],
   //   ["WebsiteDevelopment", "developer.svg"],
   //   ["Cross-platform", "developer.svg"],
   // ]);
-  return (
-    <div className="grid grid-cols-[50%_50%] py-20">
-      <Line />
-      <div className="col-auto">
-        <h2 className="mb-5 text-4xl font-bold">About me</h2>
-        <p>
-          My passion for software development started from my desire to
-          customize my home computer. I loved tinkering with OS and underlying
-          hardware, and since then, I developed a passion for learning the
-          language of the computer and how developers express that though coding
-          and software development.
-        </p>
-      </div>
-    </div>
-  );
-}
-
-function Line() {
-  const mobileIcon = ({ stack = false }: { stack: boolean }) => (
-    <FontAwesomeIcon
-      icon={faMobile}
-      size={stack ? "lg" : "2xl"}
-      className={`${stack ? "mt-[0.7rem] ml-[0.8rem] rounded-[3px] border-[0.12rem] border-gray-800 bg-gray-800" : "pr-3"}`}
-    />
-  );
-  const laptopIcon = <FontAwesomeIcon icon={faLaptop} size={"2x"} />;
-  const tabletIcon = ({ stack = false }: { stack: boolean }) => (
-    <FontAwesomeIcon
-      icon={faTablet}
-      size={stack ? "xl" : "2xl"}
-      className={`${stack ? "mt-[0.5rem] ml-[-3rem] rounded-[3px] border-[0.12rem] border-gray-800 bg-gray-800" : ""}`}
-    />
-  );
-  return (
-    <div className="col-auto">
-      <div className="ml-1.5 flex h-10 items-center gap-3 border-l-2 border-primary pl-10">
-        <FontAwesomeIcon
-          icon={faWindowMaximize}
-          size={"2xl"}
-          className="pr-3"
-        />
-        {"Website Development"}
-      </div>
-      <div className="mx-[0.22rem] my-2 size-2 rounded-full bg-primary pr-1"></div>
-      <div className="ml-1.5 flex h-10 items-center gap-4 border-l-2 border-primary pl-11">
-        {mobileIcon({ stack: false })}
-        {"App Development"}
-      </div>
-      <div className="mx-[0.22rem] my-2 size-2 rounded-full bg-primary pr-1"></div>
-      <div className="ml-1.5 flex h-10 items-center gap-2 border-l-2 border-primary pl-10">
-        <div className="flex">
-          {laptopIcon}
-          {tabletIcon({ stack: true })}
-          {mobileIcon({ stack: true })}
-        </div>
-        {"Cross-Platform Development"}
-      </div>
-    </div>
-  );
+  return <Line />;
 }
