a439a0d8a8497a88a5005cfd967593f998a47634
IAmNotARobot <38276929+spectrMeltdown@users.noreply.github.com>
2025-06-06 18:16:40 +0800

    - corrected packages - added toast - fixed contact section - removed EmailType - remove helper for EmailType

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export interface ProjectType {
   name: string;
   description: string;
@@ -7,9 +9,11 @@ export interface ProjectType {
   ghLink: string;
 }
 
-export interface EmailType {
-  name: string;
-  email: string;
-  message: string;
-  time: string;
-}
+export const contactSchema = z.object({
+  name: z.string().min(2).max(100),
+  email: z.string().min(2).max(100).email(),
+  message: z.string().min(2).max(500),
+  time: z.date(),
+});
+
+export type ContactFormData = z.infer<typeof contactSchema>;
